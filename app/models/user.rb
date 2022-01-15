eaab88eb294bb136febc887cd901f9353de9a9f2
EC2 Default User <ec2-user@ip-172-31-20-133.ec2.internal>
2022-01-15 00:52:26 +0000

    [Update] edited books and users controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,18 @@ class User < ApplicationRecord
          
   has_one_attached :image
   
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :email, presence: true
+  validates :password, presence: true
+  
   has_many :books, dependent: :destroy
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end 
+  
 end
