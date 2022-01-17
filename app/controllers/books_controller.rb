04289eb8f843077b7c271199dcb0935446689499
EC2 Default User <ec2-user@ip-172-31-20-133.ec2.internal>
2022-01-17 11:55:32 +0000

    [Update] fixed some errors

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @user = User.find(current_user.id)
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one_attached :image
+  has_one_attached :profile_image
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, presence: true, length: { minimum: 2}, length: { maximum: 20 }, uniqueness: true
   validates :email, presence: true
   validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
 
   def get_profile_image(size)
-    unless image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize: size).processed
+    profile_image.variant(resize: size).processed
   end
 end
